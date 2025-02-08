c8b57b088364f7b6191784d94b8c20ec85865c99
Rayhan-10 <rayhan17258@mail.com>
2025-02-09 02:53:43 +0600

    Learn & practice array basic

---
+++ Array/array.js
@@ -0,0 +1,55 @@
+// let numbers =[10, 20 ,30, 40];
+// let friends = ["Rayhan", "Galib", "Asif"];
+// let passed = [true , false, true];
+// let mixed = [30 , "RAyhan", 89];
+
+ //Arry length
+//accessing by index
+//  let numbers =[10, 20 ,30, 40, 70, 60, 45];
+//   console.log(numbers.length);
+//   console.log(numbers[2]);
+
+// set and get value by index
+// let Numbers=[10];
+// Numbers[1]=20;
+// Numbers[2]=30;
+// Numbers[3]=50;
+// console.log(Numbers.length);
+// console.log(Numbers[4]);
+// console.log(Numbers);
+
+//  let numbers =[10, 20 ,30, 40, 70, 60, 45];
+//  numbers[0]=90;
+//  numbers[1]=80;
+//   console.log(numbers);
+
+// let friends = ["Rayhan", "Galib", "Asif"];
+// friends[3]="Sohan";
+// friends[4]="Awal";
+// console.log(friends);
+
+//add & remove elements
+
+// let numbers =[10, 20 ,30];
+// // console.log(numbers);
+// numbers.push(40);
+// numbers.push(50);
+// numbers.push(60);
+// numbers.push(70);
+// // console.log(numbers);
+// // numbers.push(80, 90);
+// console.log(numbers);
+//  const del1= numbers.pop();
+//  const del2=numbers.pop();
+//  const del3=numbers.pop();
+// console.log(numbers);
+// console.log(del1, del2, del3);
+
+//shift & unshift
+let numbers =[40, 50 ,60];
+console.log(numbers);
+// numbers.shift();
+// console.log(numbers);
+numbers.unshift(30);
+console.log(numbers);
+
---
+++ Array/includes_indexof.js
@@ -0,0 +1,29 @@
+let friends = ["Rayhan", "awal", "Asif"];
+//includes
+// console.log(friends.includes("awal"));
+// console.log(friends.includes("galib"));
+// if(friends.includes("awal"))
+// {
+//     console.log("exists in  array");
+// }
+// else
+// {
+//     console.log("Not exists  array");
+// }
+//index_jana
+// console.log(friends.indexOf("Galib"));
+
+// let nums = [];
+// let goat = "messi neymar ronaldo";
+
+// console.log(Array.isArray(friends)); ans : true
+// console.log(Array.isArray(nums)); ans true
+// console.log(Array.isArray(goat)); ans :false 
+
+//concat arrays
+let array1 = ['apple', 'banana'];
+let array2 = ['cherry', 'date'];
+let array3 = ['Orange', 'papaya'];
+let combinedArray = array1.concat(array2).concat(array3);
+console.log(combinedArray);
+console.log(Array.isArray(combinedArray));
